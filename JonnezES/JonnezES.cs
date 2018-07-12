3ef490b72bee0ee7bbf96e59554c0a89ed1d2704
Vikanen <janne.keckman@gmail.com>
2018-07-12 15:17:36 +0300

    JonnezES: Initial Build

---
+++ JonnezES/JonnezES.cs
@@ -0,0 +1,37 @@
+﻿using MSCLoader;
+using UnityEngine;
+
+namespace JonnezES
+{
+    public class JonnezES : Mod
+    {
+        public override string ID => "JonnezESPowerUp";
+        public override string Name => "JonnezES";
+        public override string Author => "Vikanen";
+        public override string Version => "1.0";
+
+        private GameObject MOPED;
+
+        public override void OnLoad()
+        {
+            {
+                this.MOPED = GameObject.Find("JONNEZ ES(Clone)");
+                Drivetrain[] component = this.MOPED.GetComponents<Drivetrain>();
+                for (int i = 0; i < component.Length; i++)
+                {
+                    Drivetrain playMakerFSM = component[i];
+                    {
+                        playMakerFSM.revLimiter = false;
+                        playMakerFSM.maxPowerRPM = (13000);
+                        playMakerFSM.maxTorque = (14);
+                        playMakerFSM.powerMultiplier = (1.3f);
+                        playMakerFSM.externalMultiplier = (0.7f);
+                        playMakerFSM.clutchMaxTorque = (100);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
